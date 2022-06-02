262a2abe696f1b29ba83bdc3565f323230281dca
Ivan Gavryushin <keqpup232@gmail.com>
2022-06-02 12:22:40 +0300

    create cluster

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Ansible Collection - my_own_namespace.yandex_cloud_elk
+### Prepare to launch ansible
+1) You need install yc and create ssh 
+   - [manual](https://cloud.yandex.com/en/docs/cli/operations/install-cli) cli 
+   - [manual](https://cloud.yandex.ru/docs/managed-kubernetes/operations/node-connect-ssh) ssh
+2) Set your properties VM in role single_task_role/tasks or defaults
+   ```yml
+   - name: create node-clickhouse
+     my_own_module:
+       name: "node-clickhouse"
+       zone: "{{ zone }}"
+       subnet_name: "{{ subnet_name }}"
+       image_family: "{{ image_family }}"
+       path_ssh: "{{ path_ssh }}"
+       memory: 8
+       count_cores: 4
+   ```
+3) If you need download roles in playbook folder
+    ```bash
+    ansible-galaxy install -r requirements.yml -p ./roles/ -f
+    ```
+4) Change default var in clickhouse role
+   ```yaml
+   clickhouse_listen_host:
+     - "::"
 
-add file in local file system with your message
+   clickhouse_networks_default:
+     - "::/0"
+   ```
+5) Install module 
+   ```bash
+   ansible-galaxy collection build --force
+   ansible-galaxy collection install my_own_namespace-yandex_cloud_elk-2.0.0.tar.gz --force
+   ```
+6) Start playbook
+   ```bash
+   ansible-playbook site.yml ; ansible-playbook site.yml -i inventory.yml
+   ```
--- plugins/modules/my_own_module.py
+++ plugins/modules/my_own_module.py
@@ -5,6 +5,7 @@
 
 from __future__ import (absolute_import, division, print_function)
 import os.path
+import json
 
 __metaclass__ = type
 
@@ -77,8 +78,14 @@ from ansible.module_utils.basic import AnsibleModule
 
 def run_module():
     module_args = dict(
-        path=dict(type='str', required=True),
-        content=dict(type='str', required=True)
+        name=dict(type='str', required=True),
+        zone=dict(type='str', required=True),
+        subnet_name=dict(type='str', required=True),
+        image_family=dict(type='str', required=True),
+        path_ssh=dict(type='str', required=True),
+        memory=dict(type='str', required=False),
+        count_cores=dict(type='str', required=False),
+        disk_size=dict(type='str', required=False)
     )
 
     result = dict(
@@ -94,20 +101,57 @@ def run_module():
     if module.check_mode:
         module.exit_json(**result)
 
-    file_path = module.params['path']
-    content = module.params['content']
-    check_exist_file = os.path.exists(file_path)
+    command_disk_size = ' '
+    command_core_count = ' '
+    command_mem_size = ' '
+    if module.params['disk_size']:
+        command_disk_size = ' --create-disk size=' + module.params['disk_size'] + 'GB'
+    if module.params['count_cores']:
+        command_core_count = ' --cores ' + module.params['count_cores']
+    if module.params['memory']:
+        command_mem_size = ' --memory ' + module.params['memory'] + 'GB'
 
-    if check_exist_file:
-        result['changed'] = False
-        result['message'] = 'file ' + file_path + ' is created'
-    else:
-        try:
-            with open(file_path, 'w') as f:
-                f.write(content)
-            result['message'] = 'file ' + file_path + ' create success'
-        except FileNotFoundError:
-            result['message'] = "The " + file_path + " directory does not exist"
+    command = 'yc compute instance create \
+                    --name ' + module.params['name'] + ' \
+                    --zone ' + module.params['zone'] + ' \
+                    --network-interface subnet-name=' + module.params['subnet_name'] + ',nat-ip-version=ipv4 \
+                    --create-boot-disk image-folder-id=standard-images,image-family=' + module.params['image_family'] + ' \
+                    --metadata-from-file ssh-keys=' + module.params['path_ssh'] + command_mem_size + command_core_count + command_disk_size
+
+    search_name_instance = ' | ' + module.params['name']
+    os.system('yc compute instance list | grep ' + module.params['name'] + ' > list_instance.txt')
+    with open("list_instance.txt", "r") as f:
+        if search_name_instance in f.read():
+            result['changed'] = False
+            result['message'] = module.params['name'] + ' is already set'
+            module.exit_json(**result)
+        else:
+            file_log_json = 'log_'+module.params['name']+'.json'
+            os.system(command + ' --format json > ' + file_log_json)
+    os.system('rm list_instance.txt')
+    file_instance_list = 'list_' + module.params['name'] + '.txt'
+    os.system("yc compute instance list | sed -n '1p;2p;3p' > "+file_instance_list)
+    os.system('yc compute instance list | grep ' + module.params['name'] + ' >> '+file_instance_list)
+    os.system("yc compute instance list | sed  -e  '1{$q;}' -e '$!{h;d;}' -e x >> "+file_instance_list)
+    with open(file_instance_list, "r") as f:
+        result['message'] = f.read()
+
+    data = json.loads(open(file_log_json).read())
+    external_ip = data["network_interfaces"][0]["primary_v4_address"]["one_to_one_nat"]["address"]
+
+    if module.params['name']=='node-clickhouse':
+        with open("inventory.yml", "a") as f:
+            f.write('---\nclickhouse:\n  hosts:\n    ' + module.params['name'] + ':\n      '+'ansible_host: ' + external_ip + '\n')
+        with open("vector.toml", "a") as f:
+            f.write('[sources.my_source_id]\ntype = "syslog"\naddress = "0.0.0.0:514"\nmode = "tcp"\npath = "/usr/lib/systemd/system/syslog.socket"\n\n'
+                    '[sinks.my_sink_id]\ntype = "clickhouse"\ninputs = [ "my_source_id" ]\nendpoint = "http://'+ external_ip +':8123"\ndatabase = "logs"\n'
+                    'table = "access_logs"\nskip_unknown_fields = true')
+    elif module.params['name']=='node-vector':
+        with open("inventory.yml", "a") as f:
+            f.write('vector:\n  hosts:\n    ' + module.params['name'] + ':\n      '+'ansible_host: ' + external_ip + '\n')
+    elif module.params['name']=='node-lighthouse':
+        with open("inventory.yml", "a") as f:
+            f.write('lighthouse:\n  hosts:\n    ' + module.params['name'] + ':\n      '+'ansible_host: ' + external_ip + '\n')
 
     module.exit_json(**result)
 
